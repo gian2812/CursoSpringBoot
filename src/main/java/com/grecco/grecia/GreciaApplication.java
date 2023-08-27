3ba97c266a6d2e3b093e98352f81980291e7d7d3
gian grecco <giangrecco.gg@gmail.com>
2023-08-27 14:38:43 -0300

    primer commit

---
+++ src/main/java/com/grecco/grecia/GreciaApplication.java
@@ -0,0 +1,13 @@
+package com.grecco.grecia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grecco/grecia/GreciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grecco.grecia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
