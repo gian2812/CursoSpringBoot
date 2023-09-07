94ef1d5ebb5401b1040ac7db82c36c0fb5b55e87
gian grecco <giangrecco.gg@gmail.com>
2023-09-07 13:07:48 -0300

    [Fin seccion 1 Curso] application properties and endpoints

--- src/main/java/com/grecco/grecia/rest/HelloWorldController.java
+++ src/main/java/com/grecco/grecia/rest/HelloWorldController.java
@@ -1,14 +1,48 @@
 package com.grecco.grecia.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	public String getCoachName() {
+		return coachName;
+	}
+
+	public void setCoachName(String coachName) {
+		this.coachName = coachName;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "INDEX";
+	}
 
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello World";
+    }
+    
+    @GetMapping("/team")
+    public String teamInfo(){
+    	return  coachName + " es el coach de: " + teamName ;
     }
 }
